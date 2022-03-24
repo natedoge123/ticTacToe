fe3d5e973dcebb721a5449c0baa099d20e31d675
natedoge123 <98nhartley@gmail.com>
2022-03-23 20:27:43 -0400

    board spaces are working when clicked and win conditions work. Still need to add reset button for win and better win message display

--- javaScript.js
+++ javaScript.js
@@ -4,15 +4,27 @@ const playArea = document.querySelector('#playArea');
 
 // Tic Tac Toe Function
   // Modules
-const gameBoard = (() => {
-  const board = [,,,,,,,,,];
-
-  return {board };
-})();
-
 
 const gameMaster = (() => {
-  const gameWin = (board, piece) => {
+  const board = [,,,,,,,,,];
+
+  const yourTurn = (board) => {
+    let numOfX = 0;
+    let numOfO = 0;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == "x") {
+        numOfX++;
+      }
+      if (board[i] == "o") {
+        numOfO++;
+      }
+    }
+    if (numOfX == numOfO) return 0;
+    if (numOfX > numOfO) return 1;
+    if ((numOfX + numOfO) == 9) return 2;
+  }
+
+  const winConditions = (board, piece) => {
     if (board[0] == piece && board[1] == piece && board[2] == piece) return 1;
     if (board[3] == piece && board[4] == piece && board[5] == piece) return 1;
     if (board[6] == piece && board[7] == piece && board[9] == piece) return 1;
@@ -23,16 +35,34 @@ const gameMaster = (() => {
     if (board[2] == piece && board[4] == piece && board[6] == piece) return 1;
     return 0;
     }
-  const gameTie = (board) => {
-    let zero = 0;
-
+  
+  const didYouWin = (board, player) => {
+    if (winConditions(board, player.shape)) {
+      console.log(player.name + " wins!!")
+    }
 
   }
+  
   const playSpot = playArea.addEventListener('click', function(event) {
-    boardSpot = event.originalTarget.slot;
-    console.log(boardSpot);
+    boardSpot = parseInt(event.originalTarget.slot);
+    whoseTurn = yourTurn(gameMaster.board);
+
+    switch (whoseTurn) {
+      case 0: 
+        gameMaster.board[boardSpot] = player1.shape;
+        didYouWin(gameMaster.board, player1);
+        break;
+      case 1:
+        gameMaster.board[boardSpot] = player2.shape;
+        didYouWin(gameMaster.board, player2);
+        break;
+      case 2:
+        let zero = 0;
+        break;
+    }
+    boxMaker(gameMaster.board);
   });
-  return {gameWin, playArea };
+  return {board, yourTurn, winConditions, didYouWin,  playArea };
 
 })();
 
@@ -40,10 +70,6 @@ const gameMaster = (() => {
 
 const playerFactory = (name, shape) => {
   const move = (place) => {
-    if (gameBoard.board[place] == "undefined") {
-      gameBoard.board[place] = shape;
-
-    }
   }
   return {name, shape, move};
 };
@@ -77,7 +103,7 @@ function playerStart () {
 
 startGame.addEventListener('click', function(event) {
   let newGame = playerStart();
-  boxMaker(gameBoard.board);
+  boxMaker(gameMaster.board);
 });
 
 
